bea602bc874b76974f90b36ad97bceca7bf93d34
wey1998 <yitwey1998@hotmail.com>
2023-07-20 00:29:19 +0100

    Typescript Basic types tutorial

---
+++ Basics/build/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+let myName = "Alex";
+let age;
+let isLoading;
+let album;
+let postId;
+let regex = /\w+/g;
+age = 30;
+isLoading = false;
+function sum(a, b) {
+    return a + b;
+}
---
+++ Basics/src/main.ts
@@ -0,0 +1,13 @@
+let myName: string = "Alex"
+let age: number;
+let isLoading: boolean;
+let album: any;
+let postId: string | number;
+let regex:RegExp = /\w+/g
+
+age = 30
+isLoading = false
+
+function sum(a: number, b: number) {
+    return a + b
+}
