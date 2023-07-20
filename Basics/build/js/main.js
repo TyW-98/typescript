df97cc3753044adf7fd55435c73402ca0c1f3510
wey1998 <yitwey1998@hotmail.com>
2023-07-20 01:09:18 +0100

    Objects, arrays, enums tutorial

--- Basics/build/js/main.js
+++ Basics/build/js/main.js
@@ -10,3 +10,59 @@ isLoading = false;
 function sum(a, b) {
     return a + b;
 }
+let stringArr = ["A", "B", "C"];
+let unionArr = ["A", 24, "ewf"];
+let mixedArr = ["asdw", 2412, false];
+stringArr[1] = "D";
+stringArr.push("Last");
+unionArr[2] = 0;
+unionArr.unshift("Abc");
+let bandName = [];
+bandName.push("Band Name 1");
+// Tuple
+let tuple1;
+tuple1 = ["a", "b", 123];
+// Interface and type is almost the same for basics
+// interface TestObject {
+//     id: string,
+//     name: string,
+//     age: number,
+//     identifier: (string | number),
+//     collection: (string | number)[],
+//     active?: boolean,  // By putting "?" makes active property optional
+// }
+let object1 = {
+    id: "124aswfq",
+    // name: "Dave",
+    age: 49,
+    identifier: 12,
+    collection: ["1sac23", "1fasdf", 342]
+};
+const greetPerson = (testObject) => {
+    if (testObject.name) { //To Check if name is provided since it is set to optional
+        return `Greetings ${testObject.name.toUpperCase()}, Welcome`;
+    }
+    return "Hello!";
+};
+console.log(greetPerson(object1));
+// Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["F"] = 0] = "F";
+    Grade[Grade["E"] = 1] = "E";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.A);
+var Grade1;
+(function (Grade1) {
+    Grade1[Grade1["F"] = 1] = "F";
+    Grade1[Grade1["E"] = 2] = "E";
+    Grade1[Grade1["D"] = 3] = "D";
+    Grade1[Grade1["C"] = 4] = "C";
+    Grade1[Grade1["B"] = 5] = "B";
+    Grade1[Grade1["A"] = 6] = "A";
+})(Grade1 || (Grade1 = {}));
+console.log(Grade1.A);
--- Basics/src/main.ts
+++ Basics/src/main.ts
@@ -3,7 +3,7 @@ let age: number;
 let isLoading: boolean;
 let album: any;
 let postId: string | number;
-let regex:RegExp = /\w+/g
+let regex: RegExp = /\w+/g
 
 age = 30
 isLoading = false
@@ -11,3 +11,84 @@ isLoading = false
 function sum(a: number, b: number) {
     return a + b
 }
+
+let stringArr = ["A", "B", "C"]
+let unionArr = ["A",24,"ewf"]
+let mixedArr = ["asdw", 2412, false]
+
+stringArr[1] = "D"
+stringArr.push("Last")
+
+unionArr[2] = 0
+unionArr.unshift("Abc")
+
+let bandName: string[] = []
+bandName.push("Band Name 1")
+
+// Tuple
+let tuple1: [string, string, number];
+tuple1 = ["a", "b", 123]
+
+// Objects
+type TestObject = {
+    id: string,
+    name?: string,
+    age: number,
+    identifier: (string | number),
+    collection: (string | number)[],
+    active?: boolean,  // By putting "?" makes active property optional
+}
+
+// Interface and type is almost the same for basics
+
+// interface TestObject {
+//     id: string,
+//     name: string,
+//     age: number,
+//     identifier: (string | number),
+//     collection: (string | number)[],
+//     active?: boolean,  // By putting "?" makes active property optional
+// }
+
+let object1: TestObject = {
+    id: "124aswfq",
+    // name: "Dave",
+    age: 49,
+    identifier: 12,
+    collection: ["1sac23", "1fasdf", 342]
+}
+
+const greetPerson = (testObject: TestObject) => {
+
+    if (testObject.name) { //To Check if name is provided since it is set to optional
+        return `Greetings ${testObject.name.toUpperCase()}, Welcome`
+    }
+
+    return "Hello!"
+}
+
+console.log(greetPerson(object1))
+
+// Enums
+
+enum Grade {
+    F,
+    E,
+    D,
+    C,
+    B,
+    A,
+}
+
+console.log(Grade.A)
+
+enum Grade1 {
+    F = 1,
+    E,
+    D,
+    C,
+    B,
+    A,
+}
+
+console.log(Grade1.A)
