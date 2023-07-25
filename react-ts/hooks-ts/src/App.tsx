fe30ec7fb9036ea2f0d88b353f929944c5f7a375
wey1998 <yitwey1998@hotmail.com>
2023-07-26 00:10:49 +0100

    React typescript Hooks

--- react-ts/hooks-ts/src/App.tsx
+++ react-ts/hooks-ts/src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {
+  Fragment,
+  useState,
+  useEffect,
+  useCallback,
+  useMemo,
+  useRef,
+} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface User {
+  id: number;
+  name: string;
 }
 
-export default App
+type fiboFunc = (n: number) => number;
+
+// Using the fiboFunc type defined above the input and output type is already defined in the type.
+const fibo: fiboFunc = (n) => {
+  if (n < 2) return n;
+  return fibo(n - 1) + fibo(n - 2);
+};
+
+// Similar to the fibo function defined above
+function fib(n: number): number {
+  if (n < 2) {
+    return n;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
+}
+
+const myNum: number = 21;
+
+function App() {
+  const [count, setCount] = useState<number>(0);
+  const [users, setUsers] = useState<User[] | null>(null); // same as -> useState<User[]>([])
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  // inputRef.current.value to get the value
+
+  // Optional chaining to only log when there is a value. THis can prevent null.
+  console.log(inputRef?.current?.value);
+
+  // Not much typescript application for useEffect since not returning a value.
+  useEffect(() => {
+    console.log("mounting");
+    // clean up function
+    return () => console.log("mounting");
+  }, []);
+
+  // useCallback can be used to memorised a function. Last array is dependency array
+  const addCount = useCallback(
+    (): void => setCount((prevCount) => prevCount + 1),
+    []
+  );
+
+  // The function shows that the event type is possible a MouseEvent(mouse-click) or KeyboardEvent(keyboard input) on a HTMLButtonElement
+  const exampleEventFunction = useCallback(
+    (
+      event:
+        | React.MouseEvent<HTMLButtonElement>
+        | React.KeyboardEvent<HTMLButtonElement>
+    ): void => {
+      event.preventDefault();
+    },
+    []
+  );
+
+  // Use to memorise a value so it does not have to recalculate everytime a component re-renders.
+  const result = useMemo<number>(() => fibo(myNum), [myNum]);
+
+  return (
+    <Fragment>
+      <div className="container">
+        <h1>{count}</h1>
+        <button onClick={addCount}> + </button>
+        <h1>{result}</h1>
+        <input type="text" ref={inputRef} />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
