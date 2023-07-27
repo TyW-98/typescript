446f99189af731bb74dfb5385009b58250286ebc
wey1998 <yitwey1998@hotmail.com>
2023-07-27 23:36:00 +0100

    Add Header component

--- mini-ecommerce/src/App.tsx
+++ mini-ecommerce/src/App.tsx
@@ -1,9 +1,17 @@
-function App() {
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+
+export default function App() {
+  const [viewCart, setViewCart] = useState<boolean>(false);
+
   return (
     <>
-      <h1>Test connection</h1>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {viewCart ? <Cart /> : <ProductList />}
+      <Footer viewCart={viewCart} />
     </>
   );
 }
-
-export default App;
